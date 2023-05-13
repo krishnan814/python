930e81149f1e0c354f91637bc5005e362bd5562e
radhakrishnan <radhakrishnan@radhakrishnan-INVALID>
2023-05-13 12:28:12 +0530

    new update file

--- tr.py
+++ tr.py
@@ -1,11 +1,11 @@
 """The module's docstring"""
 # This program adds two numbers
 
-Num1 = 1.5
-Num2 = 6.3
+NUM1 = 1.5
+NUM2 = 6.3
 
 # Add two numbers
-Sum_numbers = num1 + num2
+Sum_numbers = NUM1 + NUM2
 
 # Display the sum
-B = f'The sum of {Num1} and {Num2} is {Sum_numbers}'
+B = f'The sum of {NUM1} and {NUM2} is {Sum_numbers}'
